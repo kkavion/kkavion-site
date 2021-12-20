855ebe0a3a7c3ad86a06db0edc8582238bb3aaa1
Eric Prem Raju <ericpremraju@gmail.com>
2021-12-20 18:48:47 +0530

    updoot

--- js/index.js
+++ js/index.js
@@ -87,4 +87,13 @@ $(document).ready(function(){
     });
 
 });
+$('.career-menu a').click(function (e) {
+    var tab = $(this);
+    if(tab.parent('li').hasClass('active')){
+        window.setTimeout(function(){
+            $(".tab-pane").removeClass('active');
+            tab.parent('li').removeClass('active');
+        },1);
+    }
+});
 
